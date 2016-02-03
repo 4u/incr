5f2afb736cad4fdf5ba100bf8a50b54af9a6289f
Max Nikitin <maxnik4u@gmail.com>
2016-02-04 02:56:20 +0300

    initial

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var incr = require('./lib');
+
+gulp.task('default', function() {
+  incr.compiler.setTemplate('incremental', 'npf.dom.incremental.%s(%s)');
+  incr.compiler.setTemplate('escape', 'goog.string.htmlEscape(%s)');
+  incr.compiler.setRequires([
+    'npf.dom.incremental',
+    'goog.string'
+  ]);
+
+  return gulp.src('examples/**/*.jade')
+    .pipe(incr.vinylTransform)
+    .pipe(rename({ extname: '.jade.js' }))
+    .pipe(gulp.dest('build/'));
+});
